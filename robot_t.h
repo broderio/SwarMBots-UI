0b835bf121ff77db031d97ac0f61f6bade671e43
micahwil <micahwil@umich.edu>
2023-11-03 12:55:25 -0400

    added mutexes

--- robot_t.h
+++ robot_t.h
@@ -18,8 +18,13 @@ typedef struct robot_t {
     uint8_t mac_address[MAC_LENGTH]; // The robot's MAC address for esp-now communication
 } robot_t;
 */
-class Robot{
+
+#define MAC_LENGTH 12
+
+class robot_t{
 public:
+    robot_t();
+    ~robot_t();
     // Initializes the robot
     int robot_init(mbot_params_t params, uint8_t *mac_address);
 
@@ -54,18 +59,27 @@ public:
     int robot_set_vel(float linear, float angular);
 
     // Sets the drive mode
-    int robot_set_drive_mode(nt mode);
+    int robot_set_drive_mode(int mode);
 
     // Sets the motor velocity if the drive mode is MODE_MOTOR_VEL_OL
-    int robot_set_motor_vel(robot_t *r, float left, float right);
+    int robot_set_motor_vel(float left, float right);
 
     // Sets the motor PWM if the drive mode is MODE_MOTOR_PWM
-    int robot_set_motor_pwm(robot_t *r, float left, float right);
+    int robot_set_motor_pwm(float left, float right);
 
     // Callback function for reading data from the host via USB serial
     void data_read_host_cb(void* args);
+
     private:
-    void mutex_wrapper(std::mutex);
+    std::mutex odometry_mutex;
+    std::mutex velocity_mutex;
+    std::mutex imu_mutex;
+    std::mutex encoders_mutex;
+    std::mutex motor_vel_goal_mutex;
+    std::mutex motor_vel_mutex;
+    std::mutex motor_pwm_mutex;
+    std::mutex drive_mode_mutex;
+    
     serial_pose2d_t odometry; // The robot's current odometry
     serial_twist2d_t velocity; // The robot's current velocity
     serial_mbot_imu_t imu; // The robot's current IMU values
