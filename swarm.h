e388419ba274b12320a26df83924c0378e8d3ec7
micahwil <micahwil@umich.edu>
2023-11-03 22:11:51 -0400

    swarm.h update

--- swarm.h
+++ swarm.h
@@ -2,6 +2,8 @@
 #define SWARM_H
 
 #include <robot_t.h>
+#include <vector>
+#include <unordered_map>
 
 struct USB_packet {
     uint8_t MAC_address[MAC_LENGTH];
@@ -11,6 +13,8 @@ struct USB_packet {
 
 class Swarm{
 public:
+
+    vector<robot_t> Robots; 
     /*
     What do we want to pass into inializer? Require that they pass in # of robots?
     Require that they pass in array of MAC addresses? Not sure what this will require,
@@ -18,14 +22,16 @@ public:
     */
     int Swarm_init();
 
+    int add_robot(uint8_t *MAC_address);
+
 private:
     //this mutex protects the usb structure
-
     std::mutex usb_mutex;
+
     int num_robots;
     int swarm_error;
     USB_packet usb;
-
+    std::unordered_map<string, int> MAC_to_RobotID;
     //This process gets a new packet from USB, puts it in the USB
     int usb_scan_and_route();
 
