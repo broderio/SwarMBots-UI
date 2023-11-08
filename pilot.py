b613df460fe7bab05c00adfd08538ea1d8984bcb
Broderick <broderio@umich.edu>
2023-11-07 23:44:00 -0500

    remove redundant reverse

--- pilot.py
+++ pilot.py
@@ -15,15 +15,15 @@ def mac_str_to_bytes(mac: str)->bytes:
 
 def create_pkt(mac: str) -> bytes:
     msg = bytearray()
-    msg.extend(reversed(struct.pack('B', SYNC_FLAG))) # 8 bit unsigned integer, msg[0]
+    msg.extend(struct.pack('B', SYNC_FLAG)) # 8 bit unsigned integer, msg[0]
     msg.extend(struct.pack('B', VERSION_FLAG)) # 8 bit unsigned integer, msg[1]
     msg.extend(reversed(struct.pack('H', 8))) # 16 bit unsigned integer, msg[2:4]
-    msg.extend(reversed(struct.pack('B', checksum(msg[2:4])))) # 8 bit unsigned integer, msg[4]
+    msg.extend(struct.pack('B', checksum(msg[2:4]))) # 8 bit unsigned integer, msg[4]
     msg.extend(reversed(struct.pack('H', MBOT_TIMESYNC))) # 16 bit unsigned integer, msg[5:7]
     msg.extend(reversed(struct.pack('Q', int(time.time() * 1000000)))) # 64 bit unsigned integer, msg[7:15]
-    msg.extend(reversed(struct.pack('B', checksum(msg[7:15])))) # 8 bit unsigned integer, msg[15]
+    msg.extend(struct.pack('B', checksum(msg[7:15]))) # 8 bit unsigned integer, msg[15]
     msg.extend(mac_str_to_bytes(mac)) # 6 byte MAC address, msg[16:22]
-    msg.extend(reversed(struct.pack('B', checksum(msg[16:22])))) # 8 bit unsigned integer, msg[28]
+    msg.extend(struct.pack('B', checksum(msg[16:22]))) # 8 bit unsigned integer, msg[28]
     return msg
 
 def main():
