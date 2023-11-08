81e1d5d2f8451f09290d1734e7d7eb6a88317acf
Broderick <broderio@umich.edu>
2023-11-08 16:39:51 -0500

    update packet structure

--- src/comms/comms.c
+++ src/comms/comms.c
@@ -9,14 +9,19 @@ uint8_t checksum(uint8_t* addends, int len) {
     return 255 - ( ( sum ) % 256 );
 }
 
-void read_header(int serial_port, uint8_t* header_data) {
+void read_mac_address(int serial_port, uint8_t* mac_address, uint16_t* pkt_len, uint8_t* checksum_val) {
     uint8_t trigger_val = 0x00;
     while(trigger_val != 0xff)
     {
         read(serial_port, &trigger_val, 1);
     }
-    header_data[0] = trigger_val;
-    read(serial_port, &header_data[1], ROS_HEADER_LEN - 1);
+    read(serial_port, pkt_len, 2);
+    read(serial_port, mac_address, MAC_ADDR_LEN);
+    read(serial_port, checksum_val, 1);
+}
+
+void read_header(int serial_port, uint8_t* header_data) {
+    read(serial_port, header_data, ROS_HEADER_LEN);
 }
 
 void read_message(int serial_port, uint8_t* msg_data_serialized, uint16_t message_len, char* topic_msg_data_checksum) {
@@ -24,10 +29,6 @@ void read_message(int serial_port, uint8_t* msg_data_serialized, uint16_t messag
     read(serial_port, topic_msg_data_checksum, 1);
 }
 
-void read_mac_address(int serial_port, uint8_t* mac_address, uint8_t* checksum_val) {
-    read(serial_port, mac_address, MAC_ADDR_LEN);
-    read(serial_port, checksum_val, 1);
-}
 
 // Function to validate the header
 int validate_header(uint8_t* header_data) {
@@ -57,30 +58,35 @@ int validate_mac_address(uint8_t* mac_address, uint8_t checksum_val) {
 
 void encode_msg(uint8_t* msg, int msg_len, uint16_t topic, uint8_t mac_address[12], uint8_t* msg_ser, int msg_ser_len) {
     // check to make sure lengths align
-    if (msg_len + ROS_PKG_LEN + MAC_ADDR_LEN + 1 != msg_ser_len) {
+    if (MAC_ADDR_LEN + 4 + msg_len + ROS_PKG_LEN != msg_ser_len) {
         printf("Error: The length of the serialized message array does not match the length of the message array plus packaging.\n");
         return;
     }
 
-    // add ROS packet header
+    // add mac address
     msg_ser[0] = SYNC_FLAG;
-    msg_ser[1] = VERSION_FLAG;
-    msg_ser[2] = (uint8_t) (msg_len % 255);
-    msg_ser[3] = (uint8_t) (msg_len >> 8);
-    uint8_t cs1_addends[2] = {msg_ser[2], msg_ser[3]};
-    msg_ser[4] = checksum(cs1_addends, 2);
+    msg_ser[1] = (uint8_t) ((msg_len + ROS_PKG_LEN) % 255);
+    msg_ser[2] = (uint8_t) ((msg_len + ROS_PKG_LEN) >> 8);
+    memcpy(msg_ser + 3, mac_address, MAC_ADDR_LEN);
+    uint8_t cs0_addends[MAC_ADDR_LEN + 2] = {msg_ser[1], msg_ser[2]};
+    memcpy(cs0_addends + 2, mac_address, MAC_ADDR_LEN);
+    msg_ser[9] = checksum(cs0_addends, MAC_ADDR_LEN + 2);
+
+    // add ROS packet header
+    msg_ser[10] = SYNC_FLAG;
+    msg_ser[11] = VERSION_FLAG;
+    msg_ser[12] = (uint8_t) (msg_len % 255);
+    msg_ser[13] = (uint8_t) (msg_len >> 8);
+    uint8_t cs1_addends[2] = {msg_ser[12], msg_ser[13]};
+    msg_ser[14] = checksum(cs1_addends, 2);
 
     // add topic and message
-    msg_ser[5] = (uint8_t) (topic % 255);
-    msg_ser[6] = (uint8_t) (topic >> 8);
-    memcpy(&msg_ser[ROS_HEADER_LEN], msg, msg_len);
+    msg_ser[15] = (uint8_t) (topic % 255);
+    msg_ser[16] = (uint8_t) (topic >> 8);
+    memcpy(msg_ser + 17, msg, msg_len);
     uint8_t cs2_addends[msg_len + 2];
-    cs2_addends[0] = msg_ser[5];
-    cs2_addends[1] = msg_ser[6];
+    cs2_addends[0] = msg_ser[15];
+    cs2_addends[1] = msg_ser[16];
     memcpy(cs2_addends + 2, msg, msg_len);
-    msg_ser[ROS_HEADER_LEN + msg_len] = checksum(cs2_addends, msg_len + 2);
-
-    // add mac address
-    memcpy(&msg_ser[msg_len + ROS_PKG_LEN], mac_address, MAC_ADDR_LEN);
-    msg_ser[msg_len + ROS_PKG_LEN + MAC_ADDR_LEN] = checksum(mac_address, MAC_ADDR_LEN);
+    msg_ser[17 + msg_len] = checksum(cs2_addends, msg_len + 2);
 }
