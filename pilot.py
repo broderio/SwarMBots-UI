8d36cb130f87a76dcc59e3b3e4d5dc58d692be45
Broderick <broderio@umich.edu>
2023-11-12 16:32:08 -0500

    fix endianness of packets

--- pilot.py
+++ pilot.py
@@ -16,16 +16,16 @@ def mac_str_to_bytes(mac: str)->bytes:
 def create_pkt(mac: str) -> bytes:
     msg = bytearray()
     msg.extend(struct.pack('B', SYNC_FLAG)) # 8 bit unsigned integer, msg[0]
-    msg.extend(reversed(struct.pack('H', 16))) # 16 bit unsigned integer, msg[1:3]
+    msg.extend(struct.pack('<H', 16)) # 16 bit unsigned integer, msg[1:3]
     msg.extend(mac_str_to_bytes(mac)) # 6 byte MAC address, msg[3:9]
     
     msg.extend(struct.pack('B', SYNC_FLAG)) # 8 bit unsigned integer, msg[10]
     msg.extend(struct.pack('B', VERSION_FLAG)) # 8 bit unsigned integer, msg[11]
-    msg.extend(reversed(struct.pack('H', 8))) # 16 bit unsigned integer, msg[12:14]
+    msg.extend(struct.pack('<H', 8)) # 16 bit unsigned integer, msg[12:14]
     msg.extend(struct.pack('B', checksum(msg[12:14]))) # 8 bit unsigned integer, msg[14]
     
-    msg.extend(reversed(struct.pack('H', MBOT_TIMESYNC))) # 16 bit unsigned integer, msg[15:17]
-    msg.extend(reversed(struct.pack('Q', int(time.time() * 1000000)))) # 64 bit unsigned integer, msg[17:25]
+    msg.extend(struct.pack('<H', MBOT_TIMESYNC)) # 16 bit unsigned integer, msg[15:17]
+    msg.extend(struct.pack('<Q', int(time.time() * 1000000))) # 64 bit unsigned integer, msg[17:25]
     msg.extend(struct.pack('B', checksum(msg[15:25]))) # 8 bit unsigned integer, msg[25]
     return msg
 
@@ -40,10 +40,19 @@ def main():
     macs = open('macs.txt', 'r')
     mac_list = macs.readlines()
     macs.close()
+    
+    # Read all available output from serial and print it
+    while ser.in_waiting:
+        print(ser.readline().decode('utf-8'))
+    print("\n\n")
 
     for mac in mac_list:
         pkt = create_pkt(mac)
+        print(f"Sending {len(pkt)} bytes to {mac}")
         ser.write(pkt)
+        time.sleep(0.1)
+        while ser.in_waiting:
+            print(ser.readline().decode('utf-8'))
         
 if __name__ == "__main__":
     main()
