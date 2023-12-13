d6b9f9b34cf6e73e59fb8323b285d963dee5aa96
Broderick <broderio@umich.edu>
2023-12-13 02:26:54 -0500

    last commit

--- examples/metric_tests/main.cpp
+++ examples/metric_tests/main.cpp
@@ -14,37 +14,36 @@ int main(int argc, char *argv[]) {
 
     // Create mbot object
     mbot::init(port);
-    std::vector<mbot> mbots = init_from_file<mbot>("macs.txt");
-    mbot::set_verbose(true);
-    mbot m = mbots[0];
+    // mbot::set_verbose(true);
+    mbot::set_fast(true);
+    mbot m("m", "30:30:f9:69:4e:49");
     
     std::cout << "Measuring round trip latency...\n";
     uint64_t avg = 0;   
     for (int i = 0; i < 100; i++) {
         m.set_odom(i, 0, 0);
+        std::cout << "Sent " << i << "\r";
         uint64_t start = mbot::get_time_us();
         while (m.get_odom().x != i);
         uint64_t end = mbot::get_time_us();
         avg += end - start;
     }
-    std::cout << "Round trip latency: " << avg / 100 / 1e6 << " seconds\n";
+    std::cout << "\nRound trip latency: " << avg / 100 / 1e6 << " seconds\n";
 
     std::cout << "Measuring update rate...\n";
     size_t count = 0;
-    mbot::set_on_update([&count](mbot *m) {
-        count++;
-    });
-    sleep(15);
-    mbot::set_on_update(nullptr);
-    std::cout << "Update rate: " << count / 15.0 << " Hz\n";
-
-    std::cout << "Measuring jitter...\n";
     std::vector<uint64_t> times;
-    mbot::set_on_update([&times](mbot *m) {
+    times.reserve(20000);
+    mbot::set_on_update([&count, &times](mbot *m) {
+        count++;
         times.push_back(mbot::get_time_us());
     });
-    sleep(15);
+    mbot::reset_invalid_msg_count();
+    sleep(60);
     mbot::set_on_update(nullptr);
+    std::cout << "Received " << count << " packets in 30 seconds.\n"
+              << "Received " << mbot::get_invalid_msg_count() << " invalid packets.\n"
+              << "Update rate: " << count / 60.0 << " Hz\n";
 
     double avg_diff = 0;
     for (int i = 1; i < times.size(); i++) {
@@ -57,6 +56,7 @@ int main(int argc, char *argv[]) {
         jitter += (diff - avg_diff) * (diff - avg_diff);
     }
     jitter = sqrt(jitter / (times.size() - 2));
+    std::cout << "Average time between packets: " << avg_diff << " us\n";
     std::cout << "Jitter: " << jitter << " us\n";
 
 }
